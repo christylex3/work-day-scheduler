ad022bf8d4a7eabcbc6213eb2ff7ec08a29dca5c
Christy Le <christylex3@gmail.com>
2022-07-04 13:00:00 -0400

    Added IDs to each timeblock for the proper hour and then created coloringTimeblock function to give timeblocks a color based upon past, present, and future criteria

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,31 @@
+var currentDate;
+var hours = ["9", "10", "11", "12", "1", "2", "3", "4", "5"];
+console.log(moment().hour());
 // Displays current date at top
 function displayDate () {
-    var currentDate = moment().format("dddd, MMM Do");
-    $("#currentDay").text(currentDate);
+    currentDate.textContent = moment().format("dddd, MMM Do");
+    // $("#currentDay").text(currentDate);
+}
+
+function coloringTimeblock () {
+
+    var comparedHour = moment().hour();
+
+    // if this is the hour that's happening, 
+    for (var i = 0; i > hours.length; i++) {
+        if (comparedHour !== hours[i]) {
+            var timeblock = document.querySelector("#" + comparedHour);
+            timeblock.classList.add("past");
+            timeblock.classList.remove("present");
+            timeblock.classList.remove("future");
+        } else if () {
+            
+        }
+    }
+    if (currentDate.moment() == ) {
+        // if 
+    }
 }
 
 displayDate();
+coloringTimeblock();
