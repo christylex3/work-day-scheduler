e07cdcd695ac3ef445b7ad1c1c3bf5393a5ffb5e
Christy Le <christylex3@gmail.com>
2022-07-04 20:05:00 -0400

    Created an array called hoursId and fixed timeblock's ID to match the array's name

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,26 @@
 var currentDate;
-var hours = ["9", "10", "11", "12", "1", "2", "3", "4", "5"];
-console.log(moment());
+var hours = ["9", "10", "11", "12", "13", "14", "15", "16", "17"];
+var hoursId = ["nineOClock", "tenOClock", "elevenOClock", "twelveOClock", "oneOClock", "twoOClock", "threeOClock", "fourOClock", "fiveOClock"];
+var idTimeblocks = ["#9"];
+
 // Displays current date at top
 function displayDate () {
-    currentDate = moment().format("dddd, MMM Do");
+    currentDate = moment().format("dddd, MMMM Do");
     $("#currentDay").text(currentDate);
 }
 
+// testing hour
+var testingHour = moment().hour();
+$("#testing").text("testingHour:" + testingHour);
+
+var testingHour2 = moment().format();
+var timeblock = document.querySelector("time9");
+console.log(timeblock);
+
 function coloringTimeblock () {
 
-    var currentHour = moment().hour();
+    var currentHour = moment().format();
+    
 
     // if this is the hour that's happening, 
     for (var i = 0; i > hours.length; i++) {
